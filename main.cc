628bf7178c1a6a8f4c7e869500e0ed13f126ac55
xuhaoqin.xhq <xuhaoqin.xhq@bytedance.com>
2021-08-03 12:56:53 +0800

    Revert "Merge branch 'dev_parser' of https://github.com/AlaricGilbert/kisyshot into dev_parser"
    This reverts commit e900047133048bd26aded855e67480cf6b9cf53a, reversing
    changes made to d27a6632279539fb8fd65cbd1c7cbcd3d595a88a.

--- main.cc
+++ main.cc
@@ -3,146 +3,48 @@
 
 #include "ast/syntax/syntax_ostream_writer.h"
 #include "context_manager.h"
-
-
 using namespace kisyshot::ast;
 
 int main() {
-
-    std::string_view code = R"(const int base = 16;
-
-int getMaxNum(int n, int arr[]){
-    int ret = 0;
-    int i = 0888;
-    while (i < n){
-        if (arr[i] > ret) ret = arr[i];
-        i = i + 1;
-    }
-    return ret;
-}
-
-int getNumPos(int num, int pos){
-    int tmp = 1;
-    int i = 0;
-    while (i < pos){
-        num = num / base;
-        i = i + 1;
-    }
-    return num % base;
-}
-
-void radixSort(int bitround, int a[], int l, int r){
-    int head[base] = {};
-    int tail[base] = {};
-    int cnt[base] = {};
-
-    if (bitround == -1 || l + 1 >= r) return;
-
-    {
-        int i = l;
-
-        while (i < r){
-            cnt[getNumPos(a[i], bitround)]
-                = cnt[getNumPos(a[i], bitround)] + 1;
-            i = i + 1;
-        }
-        head[0] = l;
-        tail[0] = l + cnt[0];
-
-        i = 1;
-        while (i < base){
-            head[i] = tail[i - 1];
-            tail[i] = head[i] + cnt[i];
-            i = i + 1;
-        }
-        i = 0;
-        while (i < base){
-            while (head[i] < tail[i]){
-                int v = a[head[i]];
-                while (getNumPos(v, bitround) != i){
-                    int t = v;
-                    v = a[head[getNumPos(t, bitround)]];
-                    a[head[getNumPos(t, bitround)]] = t;
-                    head[getNumPos(t, bitround)] = head[getNumPos(t, bitround)] + 1;
-                }
-                a[head[i]] = v;
-                head[i] = head[i] + 1;
-            }
-            i = i + 1;
-        }
+//TODO:  数组还有字符串，变量名有下划线开头的，把temp改成@temp。
+    std::string_view testCode = R"(int square(int x){
+       return x * x;
     }
 
-    {
-        int i = l;
-
-        head[0] = l;
-        tail[0] = l + cnt[0];
-
-        i = 0;
-        while (i < base){
-            if (i > 0){
-                head[i] = tail[i - 1];
-                tail[i] = head[i] + cnt[i];
-            }
-            radixSort(bitround - 1, a, head[i], tail[i]);
-            i = i + 1;
-        }
+    void fun(){
     }
+    int arr[2][test] = {1,2,3};
+    const int test = 2;
 
-    return;
-}
-
-
-int a[30000010];
-int ans;
-
-int main(){
-    int n = getarray(a);
-
-    starttime();
-
-    radixSort(8, a, 0, n);
-
-    int i = 0;
-    while (i < n){
-        ans = ans + i * (a[i] % (2 + i));
-        i = i + 1;
-    }
-
-    if (ans < 0)
-        ans = -ans;
-    stoptime();
-    putint(ans);
-    putch(10);
-    return 0;
-}
-)";
-
-    std::string_view testCode = R"( int main(){
-        int x = 1,k;
-        int y = 3;
-        int z = x + y * 3;
-        if( z == 0) {
+    int main(){
+        int  a[1][2][3] = {1,2,3,4,5,6};
+        int x = 1,y = 0;
+        int z = 1;
+        z =  a[x][y][z];
+        if(1) {
              y = x % 2;
         }
-        int i = 0;
-        while(i < 10){
-           i = i + 1;
-         }
-         y = square(y);
-        return 0;
-})";
 
+        while(y < 10){
+          y = y + 1;
+          continue;
+          x = x + 1;
+        }
+
+        z =  y + square(x);
+        fun();
+        put("string test");
+        return 0;
+    })";
     auto sm = std::make_shared<kisyshot::ContextManager>();
     auto ctx = sm->create(testCode, "/path/to/test.sy");
-    kisyshot::compiler::CodeGenerator gen;
-
     sm->lex(ctx->contextID);
     sm->parse(ctx->contextID);
-
-    ctx->syntaxTree->genCode(gen, nullptr);
+    sm->check(ctx->contextID);
+    kisyshot::compiler::CodeGenerator gen;
 
     std::cout << *(ctx->syntaxTree);
     std::cout << *(sm->diagnosticStream);
+    ctx->syntaxTree->genCode(gen, nullptr);
     return 0;
 }
