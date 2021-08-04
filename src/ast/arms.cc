bc9a801e8eaa7770845894f9ee851f042e004c7d
Windsrain <350500107@qq.com>
2021-08-05 01:47:46 +0800

    fix ifz bug

--- src/ast/arms.cc
+++ src/ast/arms.cc
@@ -167,6 +167,9 @@ void Arms::fillReg(Var * src, Register reg) {
     }
     if (src->type == VarType::ConstVar)
         fprintf(fp, "\tmov %s, #%s\n", regs[reg].name.c_str(), src->getName().c_str());
+    if (src->type == VarType::TempVar) {
+        fprintf(fp, "\tmov %s, %s\n", regs[reg].name.c_str(), regs[preReg].name.c_str());
+    }
 }
 
 void Arms::spillReg(Var * dst, Register reg) {
