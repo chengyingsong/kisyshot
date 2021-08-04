e51242129dc151a7c4309e634cdedd332d505cf9
AlaricGilbert <alaricgilbert@qq.com>
2021-08-04 22:21:54 +0800

    feat: print expected

--- test/test_all.cc
+++ test/test_all.cc
@@ -1,5 +1,18 @@
 #include <filesystem>
 #include <iostream>
+#include <fstream>
+std::string expected (const std::string& path){
+    std::string s;
+    std::ifstream in(path, std::ios::in | std::ios::binary);
+    if (in) {
+        in.seekg(0, std::ios::end);
+        s.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&s[0], long(s.size()));
+        in.close();
+    }
+    return s;
+}
 int main(){
 
     for (const auto& entry: std::filesystem::directory_iterator("cases/function_test2020")) {
@@ -7,10 +20,12 @@ int main(){
             std::cout << "file: " << entry.path().string() << std::endl;
             std::string p = entry.path().string();
             std::string i = p.replace(p.find(".S"), 2, "");
+            std::string out = p.replace(p.find(".S"), 2, ".out");
+
             system(("gcc " + entry.path().string() + " libsysy.a -o " + p).c_str());
 
             int ret = system(("./" + p).c_str());
-            std::cout << "result: " << ret;
+            std::cout << "result: " << ret << ", and expected: " << expected(out);
             std::cout << std::endl << std::endl << std::endl;
         }
     }
