a86d99a6e5e9f3086cc79fdd735daf4db8fea6f7
xuhaoqin.xhq <xuhaoqin.xhq@bytedance.com>
2021-08-05 15:43:59 +0800

    gen files as dictionary order

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <filesystem>
+#include <set>
 
 #include "ast/syntax/syntax_ostream_writer.h"
 #include "context_manager.h"
@@ -12,18 +13,22 @@ using namespace kisyshot::ast;
 
 int main() {
     auto sm = std::make_shared<kisyshot::ContextManager>();
+    std::set<std::string> paths;
     for (const auto& entry: std::filesystem::directory_iterator("cases/function_test2020")) {
         if(entry.path().extension() == ".sy") {
-            std::cout << entry.path() << std::endl;
-            auto ctx = sm->load(entry.path().string());
-            sm->lex(ctx->contextID);
-            sm->parse(ctx->contextID);
-            sm->check(ctx->contextID);
-            kisyshot::compiler::CodeGenerator gen;
-            ctx->syntaxTree->genCode(gen, nullptr);
-            kisyshot::compiler::ArmCodeGenerator armgen(gen.code, ctx);
-            armgen.generateArmCode();
+            paths.insert(entry.path().string());
         }
+    }
+    for(const auto& entry: paths){
+        std::cout << entry << std::endl;
+        auto ctx = sm->load(entry);
+        sm->lex(ctx->contextID);
+        sm->parse(ctx->contextID);
+        sm->check(ctx->contextID);
+        kisyshot::compiler::CodeGenerator gen;
+        ctx->syntaxTree->genCode(gen, nullptr);
+        kisyshot::compiler::ArmCodeGenerator armgen(gen.code, ctx);
+        armgen.generateArmCode();
     }
     return 0;
 }
